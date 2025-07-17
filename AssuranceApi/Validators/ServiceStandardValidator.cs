049439c9f260e2c74b8c116464d7d526dd65826a
Richard Stockley <richards@pocketnerd.co.uk>
2025-07-17 13:54:09 +0100

    Removed null Guidance value and mandatory validation

--- AssuranceApi/Validators/ServiceStandardValidator.cs
+++ AssuranceApi/Validators/ServiceStandardValidator.cs
@@ -20,7 +20,5 @@ public class ServiceStandardValidator : AbstractValidator<ServiceStandardModel>
         RuleFor(x => x.Name).NotEmpty().MaximumLength(200);
 
         RuleFor(x => x.Description).NotEmpty().MaximumLength(1000);
-
-        RuleFor(x => x.Guidance).NotEmpty().MaximumLength(3000);
     }
 }
